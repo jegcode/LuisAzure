ea41b82b016d5d0d4d9c85c67880e621b56cc68d
Jorge Celorio <celoriobox@outlook.com>
2019-02-01 11:45:02 -0600

    Add project files.

---
+++ SpeechToText/scripts/scriptfront.js
@@ -0,0 +1,112 @@
+﻿// status fields and start button in UI
+var phraseDiv;
+var startRecognizeOnceAsyncButton;
+// subscription key and region key for speech services.
+var subscriptionKey, regionKey;
+var authorizationToken;
+var SpeechSDK;
+var recognizer;
+
+document.addEventListener("DOMContentLoaded", function () {
+    startRecognizeOnceAsyncButton = document.getElementById("startRecognizeOnceAsyncButton");
+    // subscriptionKey = document.getElementById("subscriptionKey");
+    // regionKey = document.getElementById("regionKey");
+    phraseDiv = document.getElementById("phraseDiv");
+
+    startRecognizeOnceAsyncButton.addEventListener("click", function () {
+        startRecognizeOnceAsyncButton.disabled = true;
+        phraseDiv.innerHTML = "";
+
+        var speechConfig;
+        speechConfig = SpeechSDK.SpeechConfig.fromSubscription("yoursubscriptionkey", "yourregion");
+
+        speechConfig.speechRecognitionLanguage = "en-US";
+        var audioConfig = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
+        recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
+
+        recognizer.recognizeOnceAsync(
+            function (result) {
+                startRecognizeOnceAsyncButton.disabled = false;
+                phraseDiv.innerHTML += result.text;
+                window.console.log(result);
+
+                console.log(result.privText);
+
+                var httpRequest = new XMLHttpRequest();
+                httpRequest.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 200) {
+                        console.log(this.responseText);
+                        VisualizeFormat(this.responseText);
+                    }
+                };
+
+                httpRequest.open("GET", "yourluisendpoint?q" + result.privText, true);
+                httpRequest.send();
+                recognizer.close();
+                recognizer = undefined;
+            },
+            function (err) {
+                startRecognizeOnceAsyncButton.disabled = false;
+                phraseDiv.innerHTML += err;
+                window.console.log(err);
+
+                recognizer.close();
+                recognizer = undefined;
+            });
+    });
+
+    if (!!window.SpeechSDK) {
+        SpeechSDK = window.SpeechSDK;
+        startRecognizeOnceAsyncButton.disabled = false;
+
+        document.getElementById('content').style.display = 'block';
+        document.getElementById('warning').style.display = 'none';
+
+        // in case we have a function for getting an authorization token, call it.
+        if (typeof RequestAuthorizationToken === "function") {
+            RequestAuthorizationToken();
+        }
+    }
+});
+
+
+function VisualizeFormat(responseText) {
+        loadMPI();   
+}
+
+function loadMPI() {
+
+    var url = '../../Prescription/Prescription_examples.pdf';
+    var pdfjsLib = window['pdfjs-dist/build/pdf'];
+    console.log(pdfjsLib);
+    pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+    // Asynchronous download of PDF
+    var loadingTask = pdfjsLib.getDocument(url);
+    loadingTask.promise.then(function (pdf) {
+        console.log('PDF loaded');
+        // Fetch the first page
+        var pageNumber = 1;
+        pdf.getPage(pageNumber).then(function (page) {
+            console.log('Page loaded');
+            var scale = 1;
+            var viewport = page.getViewport(scale);
+            // Prepare canvas using PDF page dimensions
+            var canvas = document.getElementById('the-canvas');
+            var context = canvas.getContext('2d');
+            canvas.height = 400;
+            canvas.width = 600;
+            // Render PDF page into canvas context
+            var renderContext = {
+                canvasContext: context,
+                viewport: viewport
+            };
+            var renderTask = page.render(renderContext);
+            renderTask.then(function () {
+                console.log('Page rendered');
+            });
+        });
+    }, function (reason) {
+        // PDF loading error
+        console.error(reason);
+    });
+}
